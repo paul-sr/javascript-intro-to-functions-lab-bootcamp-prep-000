d575afaf27f50d06cad23fbb925f886f7dd29a73
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-12 17:10:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma (string) {
-  
+  if (string.isLowerCase()) {
+    "I can\'t hear you!"
+  }
 }
