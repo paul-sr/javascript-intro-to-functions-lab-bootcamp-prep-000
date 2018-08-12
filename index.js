1fbb2315257c26868fab808de276b8adbb15c021
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-12 17:11:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma (string) {
-  if (string === string.toLowerCase()) {
-    return "I can\'t hear you!"
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
   } else {
     return string 
   }
