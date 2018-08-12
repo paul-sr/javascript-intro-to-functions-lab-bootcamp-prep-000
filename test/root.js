cf997c0ce99e0fc8a45132496e5dc4a1224f0ce9
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-12 17:02:25 +0000

    automatically backed up by learn

--- test/root.js
+++ test/root.js
@@ -4,12 +4,15 @@ const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
 
+
 before(function(done) {
   const babelResult = babel.transformFileSync(
+    lunch; pack green tea
+    Scheduled: Aug 15, 2018 at 11:30 AM to 12:00 PM
     path.resolve(__dirname, '..', 'index.js'), {
       presets: ['es2015']
     }
-  );
+  ); 
 
   const html = path.resolve(__dirname, '..', 'index.html')
 
